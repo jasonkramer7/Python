83b6d23a2f0b73092bdbbecd5072d1d49a5ebc1d
jasonkramer7 <jasonkramer7@gmail.com>
2024-08-04 18:07:59 -0500

    day 30

--- Day_26/main.py
+++ Day_26/main.py
@@ -1,8 +1,19 @@
 import pandas
 
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
-phonic_dict = {row.letter:row.code for (index, row) in data.iterrows()}
+phonic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+print(phonic_dict)
 
-word = input("Enter a word: ").upper()
-output = [phonic_dict[letter] for letter in word]
-print(output)
+
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        output = [phonic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(output)
+
+
+generate_phonetic()
